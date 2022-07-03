2b41848a993dfea514af8d88e3157b41b63d9d83
usmon6123 <usmonataboyev2000@gmail.com>
2022-07-03 16:41:41 +0500

    3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,13 +18,9 @@ public class Main {
 
         //task 2
 //        System.out.println(task2(5));
-        int count = 0;
-        for (int i = 97; i <= 122; i++) {
-            System.out.print(i + "=" + (char) i + "; ");
-            count++;
-            if (count%7==0) System.out.println();
-        }
 
+
+        System.out.println(task3("dasturchi"));
 
     }
 
@@ -46,5 +44,72 @@ public class Main {
         //TUB SONLAR RO'YHATI HAR QANCHA UZUN BO'LMASIN TARKIBIDA KO'PAYTMASI NO'LL BILAN
         //TUGAYDIGAN 1 TA SON JUFTLIK {2,5} BOR HALOS
         return n >= 5 ? 1 : 0;
+    }
+
+
+    public static String task3(String str) {
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            newStr.append(encodeStr(str.substring(i, i + 1)));
+        }
+        return String.valueOf(newStr);
+
+    }
+
+    private static String encodeStr(String s) {
+        switch (s) {
+            case "a":
+                return "t";
+            case "b":
+                return "c";
+            case "c":
+                return "d";
+            case "d":
+                return "o";
+            case "e":
+                return "p";
+            case "f":
+                return "y";
+            case "g":
+                return "z";
+            case "h":
+                return "e";
+            case "i":
+                return "f";
+            case "j":
+                return "o";
+            case "k":
+                return "p";
+            case "l":
+                return "a";
+            case "m":
+                return "b";
+            case "n":
+                return "k";
+            case "o":
+                return "l";
+            case "p":
+                return "o";
+            case "q":
+                return "p";
+            case "r":
+                return "s";
+            case "s":
+                return "t";
+            case "t":
+                return "e";
+            case "u":
+                return "f";
+            case "v":
+                return "y";
+            case "x":
+                return "i";
+            case "y":
+                return "l";
+            case "z":
+                return "m";
+        }
+
+        return s;
     }
 }
